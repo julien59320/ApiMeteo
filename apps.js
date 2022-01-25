db7512b4743646d539fb0ffaf4eda36032b6bc03
julien spileers <spileers.julien59@gmail.com>
2022-01-25 16:04:32 +0100

    remise a zero

--- apps.js
+++ apps.js
@@ -1,5 +1,14 @@
 
 
+const weatherIcons = {
+    "Rain": "wi wi-day-rain",
+    "Clouds": "wi wi-day-cloudy",
+    "Clear": "wi wi-day-sunny",
+    "Snow": "wi wi-day-snow",
+    "mist": "wi wi-day-fog",
+    "Drizzle": "wi wi-day-sleet",
+}
+
 function capitalise(str){
     return str[0].toUpperCase() + str.slice(1);
 }
@@ -32,11 +41,11 @@ async function main(withIP = true){
     }
     //! 3) Chopper les infos meteo de la ville
     //? Appel de mon API pour voir le temp dans la ville/ Await attend effectuer la fonction pour faire la suivante
-    const meteo = await fetch(`http://api.weatherstack.com/current?access_key=710a5ea8d04a151e67f1208874f1992a&query=${ville}`)
+    const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=frunits=metric`)
     //? transformer le  resultat en json
     .then(resultat => resultat.json())
     //? Recuperation de la meteo
-    .then(json => json);
+    .then(json => json)
 
      
 
@@ -45,20 +54,17 @@ async function main(withIP = true){
 }
 //!) Afficher les informations 
 function displayWeatherInfos(data){
-    // console.log(data);
-    const name = data.location.name;
-    const temperature = data.current.temperature;
-    const conditions = data.current.weather_icons[0];
-    const description = data.current.weather_descriptions[0];
-    // console.log(conditions);
+    const name = data.name;
+    const temperature = data.main.temp;
+    const conditions = data.weather[0].main;
+    const description = data.weather[0].description;
 
     document.querySelector('#ville').textContent = name;
     document.querySelector('#temperature').textContent = Math.round(temperature);
     document.querySelector('#conditions').textContent = capitalise(description);
-    document.querySelector('#image').src = conditions;
+    document.querySelector('i.wi').className = weatherIcons[conditions];
 
-    document.body.className = description.toLowerCase();
-    console.log(description);
+    document.body.className = conditions.toLowerCase();
 }
 
 //!) Rendre la ville editable
